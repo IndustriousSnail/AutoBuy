6dfd9557870907e1554c2f004cd39494eb0a9000
924634262@qq.com <zhaohongfei>
2019-06-16 22:03:21 +0800

    登录

--- shop/jd.py
+++ shop/jd.py
@@ -8,31 +8,39 @@
 
 from selenium import webdriver
 
+
+class JD(object):
+
+    def __init__(self):
+        options = webdriver.ChromeOptions()
+        # options.add_argument("--incognito")  # 隐身窗口
+
+        driver = webdriver.Chrome(executable_path="../plugins/chromedriver.exe", chrome_options=options)
+        driver.implicitly_wait(30)
+        driver.maximize_window()  # 将窗口最大化
+        driver.get("http://www.jd.com")
+        self.driver = driver
+
+    def login(self):
+        driver = self.driver
+        login_link = driver.find_element_by_id("ttbar-login")  # 获取登录链接
+
+        login_link.click()  # 点击登录按钮
+
+        login_tab = driver.find_element_by_link_text("账户登录")
+        login_tab.click()
+
+        login_name_text = driver.find_element_by_id("loginname")  # 账号
+        login_name_text.clear()
+        login_name_text.send_keys("login_name")
+
+        login_password = driver.find_element_by_id("nloginpwd")  # 密码
+        login_password.clear()
+        login_password.send_keys("password")
+
+        login_button = driver.find_element_by_link_text("登    录")
+        login_button.click()
+
+
 if __name__ == '__main__':
-    options = webdriver.ChromeOptions()
-    options.add_argument("--incognito")
-
-    driver = webdriver.Chrome(executable_path="../plugins/chromedriver.exe", chrome_options=options)
-    driver.implicitly_wait(30)
-    driver.maximize_window()  # 将窗口最大化
-
-    driver.get("http://www.jd.com")
-
-    login_link = driver.find_element_by_id("ttbar-login")  # 获取登录链接
-
-    login_link.click()  # 点击登录按钮
-
-    login_tab = driver.find_element_by_link_text("账户登录")
-    login_tab.click()
-
-    login_name_text = driver.find_element_by_id("loginname")  # 账号
-    login_name_text.clear()
-    login_name_text.send_keys("login_name")
-
-    login_password = driver.find_element_by_id("nloginpwd")  # 密码
-    login_password.clear()
-    login_password.send_keys("password")
-
-    login_button = driver.find_element_by_link_text("登    录")
-    login_button.click()
-
+    JD()
