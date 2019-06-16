7804b4a35696c98a5d7bdcf7c08eb4d5bd2d34dc
924634262@qq.com <zhaohongfei>
2019-06-16 21:55:48 +0800

    登录

--- shop/jd.py
+++ shop/jd.py
@@ -16,4 +16,23 @@ if __name__ == '__main__':
     driver.implicitly_wait(30)
     driver.maximize_window()  # 将窗口最大化
 
-    driver.get("http://www.baidu.com/")
+    driver.get("http://www.jd.com")
+
+    login_link = driver.find_element_by_id("ttbar-login")  # 获取登录链接
+
+    login_link.click()  # 点击登录按钮
+
+    login_tab = driver.find_element_by_link_text("账户登录")
+    login_tab.click()
+
+    login_name_text = driver.find_element_by_id("loginname")  # 账号
+    login_name_text.clear()
+    login_name_text.send_keys("login_name")
+
+    login_password = driver.find_element_by_id("nloginpwd")  # 密码
+    login_password.clear()
+    login_password.send_keys("password")
+
+    login_button = driver.find_element_by_link_text("登    录")
+    login_button.click()
+
